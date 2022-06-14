f19ac7454aec2ebb912008a9cbbe5629939f9d53
Kyle Butts <buttskyle96@gmail.com>
2022-06-14 15:05:30 -0600

    FUOC bug fix

--- pages/_document.js
+++ pages/_document.js
@@ -14,6 +14,7 @@ export default function Document() {
         <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100;0,300;0,700;0,800;1,300&family=Permanent+Marker&display=swap" rel="stylesheet" /> 
       </Head>
       <body className="transition-all duration-300">
+        <script>0</script>
         <Main />
         <NextScript />
       </body>
